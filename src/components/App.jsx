4a0a8d976fe100ba780d8899a34e5f1f775278ad
HnatenkoOlya <gnatenkoolgaig1210@gmail.com>
2025-03-15 15:06:31 +0100

    1.3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+/*import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/vite.svg'*/
 import './App.css'
 
-function App() {
+/*function App() {
   const [count, setCount] = useState(0)
 
   return (
@@ -32,4 +32,38 @@ function App() {
   )
 }
 
-export default App
+export default App*/
+/*function Product() {
+    return (
+      <div>
+        <h2>Cookies</h2>
+      <p>Price: 999 credits</p>
+      </div>
+    );
+  };
+  
+  export default function App() {
+    return (
+      <div>
+        <h1>Products</h1>
+  
+        <Product />
+        <Product />
+        <Product />
+      </div>
+    );
+  }*/
+
+  import Product from './Product';
+
+  export default function App() {
+    return (
+      <div>
+        <h1>Best selling</h1>
+  
+        <Product />
+        <Product />
+        <Product />
+      </div>
+    );
+  }  
