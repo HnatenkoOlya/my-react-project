980ef086946fcd2d17cc6b49185eb73be38e9f97
HnatenkoOlya <gnatenkoolgaig1210@gmail.com>
2025-03-17 16:39:19 +0100

    1.4

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,20 @@
-export default function Product() {
+export default function Product(props) {
     return (
       <div>
-        <h2>Tacos</h2>
-        <p>Price: 999 credits</p>
+        <h2>{props.name}</h2>
+        <img src={props.imgUrl} alt="props.name" width="640" />
+        <p>Price: {props.price} credits</p>
       </div>
     );
   };
+  //Або
+
+/*export default function Product({ name, imgUrl, price }) {
+  return (
+    <div>
+      <h2>{name}</h2>
+	  <img src={imgUrl} alt={name} width="480" />
+	  <p>Price: {price} credits</p>
+    </div>
+  );
+};*/
